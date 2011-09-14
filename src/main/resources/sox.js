63ae7028983832469edbb7d3b08c8101303bebd2
softprops <d.tangren@gmail.com>
2011-09-14 03:02:00 -0400

    fix folding of sox settings

--- src/main/resources/sox.js
+++ src/main/resources/sox.js
@@ -1,11 +1,8 @@
 (function($){
   $(function(){
     $("html").attr({"data-useragent":  navigator.userAgent, "data-platform": navigator.platform });
-    //$(document).bind('keyup', function(e){
-    //  if((e.keyCode || e.which) == 91/*c*/) { $("div.content").toggle(); }
-    //});
     $("h1 a").bind('click', function(e){
-      $(this).find("div.content").toggle();
+      $(this).parent().siblings("div.content").toggle();
     });
   });
 })(jQuery);

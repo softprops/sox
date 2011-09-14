867eee95a110b315ac7600991a0215062fd7f728
softprops <d.tangren@gmail.com>
2011-09-14 02:54:10 -0400

    now 100% more useful

--- src/main/scala/sox.scala
+++ src/main/scala/sox.scala
@@ -14,12 +14,25 @@ object NoSettings extends SettingFilter {
   def apply(s: SoxSetting) = false
 }
 
+case class OnlyIn(config: String) extends SettingFilter {
+  def apply(s: SoxSetting) =
+    s.key.scope.config.foldStrict(_.name, "", "").equalsIgnoreCase(config)
+}
+
+// sbt generates one of these for every task
+// non-informative
+object IgnoreConfiguration extends SettingFilter {
+  val Name = "configuration"
+  def apply(s: SoxSetting) =
+    !Name.equalsIgnoreCase(s.key.key.label)
+}
+
 object Keys {
    val stitch = InputKey[Unit]("stitch",
                               "Compiles a list of sbt Settings and stitches them together into a browsable document")
-   val soxDirectory = SettingKey[java.io.File]("sox-directory","Directory where sox docs will be written to")
-   val preview  = TaskKey[Unit]("preview", "Generates a sox doc and launches in a browser")
-   val filter = SettingKey[SettingFilter]("filter", "Filters which Settings get included")
+   val drawer = SettingKey[java.io.File]("drawer","Directory where sox docs will be written to")
+   val filter = SettingKey[SettingFilter]("filter",
+                                          "Filters which Settings get included. Defaults to every setting exception those named `configuration`")
 }
 
 case class SoxSetting(key: sbt.Project.ScopedKey[_],
@@ -40,11 +53,11 @@ object Plugin extends sbt.Plugin {
   import Project.ScopedKey
 
   def options: Seq[Setting[_]] = inConfig(Sox)(Seq(
-    soxDirectory <<= (crossTarget, configuration) { (outDir, conf) => outDir / (Defaults.prefix(conf.name) + "sox") },
-    filter := AllSettings,
+    drawer <<= (crossTarget, configuration) { (outDir, conf) => outDir / (Defaults.prefix(conf.name) + "sox") },
+    filter := IgnoreConfiguration,
     stitch <<= InputTask(_ => (Space ~> ID).?) {
-      (scope: TaskKey[Option[String]]) =>
-        (scope, state, configuration, soxDirectory, filter, streams) map { (scope, state, config, sd, sf, out) =>
+      (fscope: TaskKey[Option[String]]) =>
+        (fscope, state, configuration, drawer, filter, streams) map { (fscope, state, config, sd, sf, out) =>
 
           val extracted: Extracted = Project.extract(state)
           val sessionSettings = extracted.session.original
@@ -55,28 +68,33 @@ object Plugin extends sbt.Plugin {
             val scope = key.scope
             val scoped = ScopedKey(scope, key.key)
             structure.data.definingScope(scope, key.key) match {
-			         case Some(providedBy) =>
+			        case Some(providedBy) =>
 
-                 val cMap: Project.CompiledMap/*Map[ScopedKey[_], Compiled[_]]*/ =
-                 Project.compiled(structure.settings, true/*actual*/)(structure.delegates, structure.scopeLocal)
+                val cMap: Project.CompiledMap/*Map[ScopedKey[_], Compiled[_]]*/ =
+                  Project.compiled(structure.settings, true/*actual*/)(structure.delegates, structure.scopeLocal)
 
-		             val related: Iterable[Project.ScopedKey[_]] = cMap.keys.filter(k => k.key == key && k.scope != scope)
+		            val related: Iterable[Project.ScopedKey[_]] = cMap.keys.filter(k => k.key == key && k.scope != scope)
 
-                 val depends: Iterable[sbt.Project.ScopedKey[_]] = cMap.get(scoped) match {
-                   case Some(c) => c.dependencies.toSet
-                   case None => Set.empty
-                 }
+                val depends: Iterable[sbt.Project.ScopedKey[_]] = cMap.get(scoped) match {
+                  case Some(c) => c.dependencies.toSet
+                  case None => Set.empty
+                }
 
-		             val reverse: Iterable[sbt.Project.ScopedKey[_]] = Project.reverseDependencies(cMap, scoped)
+		            val reverse: Iterable[sbt.Project.ScopedKey[_]] = Project.reverseDependencies(cMap, scoped)
 
-                 Some(SoxSetting(key, providedBy, depends, reverse, Project.delegates(structure, scope, key.key), related))
+                Some(SoxSetting(key, providedBy, depends, reverse, Project.delegates(structure, scope, key.key), related))
 
-			         case None => None
-		       }
+			        case None => None
+		        }
          }
 
          val to = new java.io.File(sd, "sox.html")
-         IO.write(to, Template(defined.filter(sf)))
+         (defined.filter(sf), fscope) match {
+           case (sx, None) =>
+             IO.write(to, Template(sx))
+           case (sx, Some(fc)) =>
+             IO.write(to, Template(sx.filter(OnlyIn(fc))))
+         }
          Seq("sox.css", "jquery.min.js", "sox.js") foreach { r =>
            val f = new java.io.File(sd, r)
            IO.delete(f)

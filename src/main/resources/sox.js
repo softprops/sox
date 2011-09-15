dfde41d7eea6525f335ae4002801802cf0015fa4
softprops <d.tangren@gmail.com>
2011-09-15 02:00:58 -0400

    type 's' to hide extra info and 'shift+s' to reveal it again

--- src/main/resources/sox.js
+++ src/main/resources/sox.js
@@ -4,5 +4,14 @@
     $("h1 a").bind('click', function(e){
       $(this).parent().siblings("div.content").toggle();
     });
+    $(document).bind('keyup', function(e) {
+        if(e.which === 83/*s*/) {
+            if(e.shiftKey) {
+                $('div.content').show();
+            } else {
+                $('div.content').hide();
+            }
+        }
+    });
   });
 })(jQuery);
